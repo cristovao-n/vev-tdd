2bba0ba508c4862d4b6b7e3514fc87e86a317e1e
ismaelneto49 <ismaelneto49@gmail.com>
2024-07-31 19:34:42 -0300

    test: add ticket tests red bar

---
+++ tickets-system/test/entities/TicketLotTest.java
@@ -0,0 +1,65 @@
+package entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TicketLotTest {
+
+    private TicketLot ticketLot;
+
+    private Ticket makeTicket(Long id, TicketType type, TicketStatus status) {
+        return new Ticket.Builder()
+                .id(id)
+                .type(type)
+                .status(status)
+                .build();
+    }
+
+    private TicketLot makeEmptyTicketLot(Long id, List<Ticket> tickets, double discount, double normalTicketPrice) {
+        return new TicketLot.Builder()
+                .id(id)
+                .tickets(tickets)
+                .discount(discount)
+                .normalTicketPrice(normalTicketPrice)
+                .build();
+    }
+
+    @BeforeEach
+    void init() {
+        List<Ticket> tickets = new ArrayList<>();
+        tickets.add(this.makeTicket(0, TicketType.VIP, TicketStatus.NOT_SOLD));
+        tickets.add(this.makeTicket(0, TicketType.HALF, TicketStatus.NOT_SOLD));
+        tickets.add(this.makeTicket(0, TicketType.NORMAL, TicketStatus.NOT_SOLD));
+        this.ticketLot = this.makeEmptyTicketLot(0L, tickets, 0.25, 10);
+    }
+
+    @Test
+    void testTicketPrices() {
+        assertEquals(20, this.ticketLot.getVIPTicketPrice());
+        assertEquals(10, this.ticketLot.getNormalTicketPrice());
+        assertEquals(5, this.ticketLot.getHalfTicketPrice());
+    }
+
+    @Test
+    void testDiscountGreaterThan25() {
+        assertThrows(RuntimeException.class, () -> this.makeEmptyTicketLot(0L, Collections.emptyList(), 0.26, 10));
+    }
+
+    @Test
+    void testDiscountEqualTo25() {
+        TicketLot test = this.makeEmptyTicketLot(0L, Collections.emptyList(), 0.25, 10);
+        assertEquals(0.25, test.getDiscount());
+    }
+
+    @Test
+    void testDiscountLessThan25() {
+        TicketLot test = this.makeEmptyTicketLot(0L, Collections.emptyList(), 0.24, 10);
+        assertEquals(0.24, test.getDiscount());
+    }
+}
