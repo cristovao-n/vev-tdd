64699622a4819f93403cac1b1a4b775747c0ee9c
ismaelneto49 <ismaelneto49@gmail.com>
2024-07-31 22:15:37 -0300

    chore: add changes to greenbar tests

--- tickets-system/main/entities/Show.java
+++ tickets-system/main/entities/Show.java
@@ -21,12 +21,113 @@ public class Show {
         this.isInSpecialDate = builder.isInSpecialDate;
     }
 
+    public Double getFee() {
+        return this.fee;
+    }
+
+    public Double getInfrastructureExpenses() {
+        return this.infrastructureExpenses;
+    }
+
     public List<TicketLot> getTicketLots() {
         return new ArrayList<>(this.ticketLots);
     }
 
     public void setTicketLots(List<TicketLot> ticketLots) {
         this.ticketLots = ticketLots;
+    }
+
+    public Report generateReport() {
+        int VIPTicketsSold = 0;
+        int halfTicketsSold = 0;
+        int normalTicketsSold = 0;
+        double ticketsTotalPrice = 0;
+
+        for (TicketLot ticketLot : ticketLots) {
+            for (Ticket ticket : ticketLot.getTickets()) {
+                if (ticket.getStatus().equals(TicketStatus.SOLD)) {
+                    switch (ticket.getType()) {
+                        case VIP -> {
+                            ticketsTotalPrice += ticketLot.getVIPTicketPrice();
+                            VIPTicketsSold++;
+                        }
+                        case HALF -> {
+                            ticketsTotalPrice += ticketLot.getHalfTicketPrice();
+                            halfTicketsSold++;
+                        }
+                        case NORMAL -> {
+                            ticketsTotalPrice += ticketLot.getNormalTicketPrice();
+                            normalTicketsSold++;
+                        }
+                    }
+                }
+            }
+        }
+
+        double infrastructureExpenses = isInSpecialDate ?
+                getInfrastructureExpenses() * 1.15 :
+                getInfrastructureExpenses();
+
+        double showIncome = ticketsTotalPrice -
+                infrastructureExpenses -
+                getFee();
+
+        ShowStatus status = null;
+        if (showIncome > 0) {
+            status = ShowStatus.PROFIT;
+        } else if (showIncome == 0) {
+            status = ShowStatus.STABLE;
+        } else if (showIncome < 0) {
+            status = ShowStatus.LOSS;
+        }
+        return new Report(
+                VIPTicketsSold,
+                halfTicketsSold,
+                normalTicketsSold,
+                showIncome,
+                status
+        );
+    }
+
+    public class Report {
+        private int VIPTicketsSold;
+        private int halfTicketsSold;
+        private int normalTicketsSold;
+        private double showIncome;
+        private ShowStatus showStatus;
+
+        public Report(int VIPTicketsSold,
+                      int halfTicketsSold,
+                      int normalTicketsSold,
+                      double showIncome,
+                      ShowStatus showStatus
+        ) {
+            this.VIPTicketsSold = VIPTicketsSold;
+            this.halfTicketsSold = halfTicketsSold;
+            this.normalTicketsSold = normalTicketsSold;
+            this.showIncome = showIncome;
+            this.showStatus = showStatus;
+        }
+
+        public int getVIPTicketsSold() {
+            return this.VIPTicketsSold;
+        }
+
+        public int getHalfTicketsSold() {
+            return this.halfTicketsSold;
+        }
+
+        public int getNormalTicketsSold() {
+            return this.normalTicketsSold;
+        }
+
+        public double getShowIncome() {
+            return this.showIncome;
+        }
+
+        public ShowStatus getShowStatus() {
+            return this.showStatus;
+        }
     }
 
     public static class Builder {
---
+++ tickets-system/main/entities/ShowStatus.java
@@ -0,0 +1,9 @@
+package entities;
+
+public enum ShowStatus {
+    PROFIT("Show made profit"),
+    STABLE("Show did not lose money"),
+    LOSS("Show lost money");
+
+    ShowStatus(String description) {}
+}
